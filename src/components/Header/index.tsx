9f5a8c8f489acfe6a7419cd0d8ae6a527bfc5043
WeslleySOR <weslleywruas@gmail.com>
2022-06-14 21:00:30 -0300

    finalized

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,19 @@
-import logoImg from '../../assets/logo.svg';
+import logoImg from "../../assets/logo.svg";
+import { AiOutlinePlus } from "react-icons/ai";
 
-import { Container, Content } from './styles';
+import { Container, Content } from "./styles";
 
 interface HeaderProps {
-  onOpenNewTransactionModal: () => void;
+	onOpenNewTransactionModal: () => void;
 }
 
 export const Header = ({ onOpenNewTransactionModal }: HeaderProps) => (
-  <Container>
-    <Content>
-      <img src={logoImg} alt="dt money" />
-      <button type="button" onClick={onOpenNewTransactionModal}>
-        Nova Transação
-      </button>
-    </Content>
-  </Container>
+	<Container>
+		<Content>
+			<img src={logoImg} alt="dt money" />
+			<button type="button" onClick={onOpenNewTransactionModal}>
+				Nova Transação <AiOutlinePlus />
+			</button>
+		</Content>
+	</Container>
 );
